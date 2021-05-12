3093f1366f63b4391fcc3fe05045ff88d2c8d116
RNtaate <royhyde14@gmail.com>
2021-05-13 01:56:22 +0300

    Use mapDispatchToProps to access redux store in App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
-function App() {
+import YearSelect from './components/YearSelect';
+import GenreSelect from './components/GenreSelect';
+import fetchGenreList from './Helpers/FetchMethods';
+import { API_KEY } from './Helpers/HelperConstants';
+import getGenresListAction from './actions/index';
+
+function App(props) {
+
+  const [genresLoaded, setGenresLoaded] = useState(false);
+
+  useEffect(() => {
+    fetchGenreList(API_KEY).then((result) => {
+      props.getMyGenresList(result);
+      setGenresLoaded(true);
+    }).catch((e) => {
+      console.log('Something went horribly wrong and I some how');
+    })
+  }, [])
+
   return (
     <div className="App">
-      <h1>This is the app component</h1>
+      <YearSelect />
+      {genresLoaded ? <GenreSelect /> : <p>No Genres Yet</p>}
     </div>
   );
 }
 
-export default App;
+let mapDispatchToProps = (dispatch) => {
+  return {
+    getMyGenresList : (genresObj) => {
+      dispatch(getGenresListAction(genresObj));
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(App);
