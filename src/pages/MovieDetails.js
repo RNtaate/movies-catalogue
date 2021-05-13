c5858bc6de5607c69d27e250ed0c83352bad83b2
RNtaate <royhyde14@gmail.com>
2021-05-14 02:56:28 +0300

    Create MovieDetails page and component

---
+++ src/pages/MovieDetails.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+let MovieDetails = () => {
+  return (
+    <div className="movie-details-div">
+      <div className="movie-content-div">
+        <h2>Movie Title</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, maxime hic. Neque, vero. Suscipit ab doloribus id nisi. Maiores, dicta totam, aspernatur assumenda rerum laboriosam consectetur voluptatum impedit temporibus nam nisi accusamus mollitia ipsa esse. Rerum incidunt molestiae voluptatem, aut aliquid nemo facilis, vitae tenetur alias, odit iste. Placeat eaque, fuga voluptatem architecto doloribus quasi autem expedita mollitia facilis dolorum!</p>
+        <p>Starring: Roy Ntaate, Guy Gustave</p>
+        <p>Genre: Action, Comedy, Adventure</p>
+        <p>Rating: 8.7</p>
+      </div>
+    </div>
+  )
+}
+
+export default MovieDetails;
