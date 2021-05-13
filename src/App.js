01b3ebb0a6d6230961d121632186a0e8776938d0
RNtaate <royhyde14@gmail.com>
2021-05-13 04:56:59 +0300

    Implement fetching of movies from API, Reading redux store

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import './App.css';
 
 import YearSelect from './components/YearSelect';
 import GenreSelect from './components/GenreSelect';
-import fetchGenreList from './Helpers/FetchMethods';
+import {fetchGenreList, fetchMoviesList} from './Helpers/FetchMethods';
 import { API_KEY } from './Helpers/HelperConstants';
-import getGenresListAction from './actions/index';
+import {getGenresListAction, getMoviesListAction} from './actions/index';
+import MovieCard from './components/MovieCard';
 
 function App(props) {
 
   const [genresLoaded, setGenresLoaded] = useState(false);
+  const [moviesLoaded, setMoviesLoaded] = useState(false);
+
+  const [localListFilters, setLocalListFilters] = useState({
+    year: 2021,
+    genres: '28|35'
+  })
+
+  let {moviesObject, genresObject} = props
 
   useEffect(() => {
     fetchGenreList(API_KEY).then((result) => {
@@ -19,22 +28,46 @@ function App(props) {
     }).catch((e) => {
       console.log('Something went horribly wrong and I some how');
     })
+
+
+    fetchMoviesList(API_KEY, localListFilters.year, localListFilters.genres)
+    .then((result) => {
+      props.getMyMoviesList(result);
+      setMoviesLoaded(true);
+    }).catch((e) => {
+      console.log('Something went horribly wrong!!!')
+    })
   }, [])
 
   return (
     <div className="App">
       <YearSelect />
       {genresLoaded ? <GenreSelect /> : <p>No Genres Yet</p>}
+      <div>
+        {moviesLoaded ? moviesObject.results.map((movie) => <MovieCard movie={movie} genresObject={genresObject}/>) : <p>No movies Yet</p>}
+      </div>
     </div>
   );
 }
+
+let mapStateToProps = (state) => {
+  return {
+    genresObject : state.genreListReducer,
+    moviesObject : state.moviesListReducer
+  }
+}
+
 
 let mapDispatchToProps = (dispatch) => {
   return {
     getMyGenresList : (genresObj) => {
       dispatch(getGenresListAction(genresObj));
+    },
+
+    getMyMoviesList : (moviesObj) => {
+      dispatch(getMoviesListAction(moviesObj));
     }
   }
 }
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
