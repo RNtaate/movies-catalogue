3ee5a216cca26e08af9b0e734cfdf30d8bc9f38a
RNtaate <royhyde14@gmail.com>
2021-05-13 13:08:32 +0300

    Update MoviesListReducer

--- src/reducers/MoviesList.js
+++ src/reducers/MoviesList.js
@@ -3,7 +3,7 @@ import {moviesListInitialState} from './InitialStates';
 let MoviesListReducer = (state = moviesListInitialState, action) => {
   switch(action.type) {
     case 'GET_MOVIES_LIST':
-      return action.payLoad;
+      return { ...state, movies: state.movies.concat(action.payLoad.results)};
     default: 
       return state;
   }
