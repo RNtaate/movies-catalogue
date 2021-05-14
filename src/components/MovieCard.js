1b34155edafd4173a9d34841497c743641675b6f
RNtaate <royhyde14@gmail.com>
2021-05-14 14:49:37 +0300

    Add link element pointing to MovieDetails page

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import {convertToGenreNames} from '../Helpers/HelperMethods';
+import {Link} from 'react-router-dom';
 
 let MovieCard = (props) => {
   const {movie, genresObject} = props;
 
   let movieGenreNames = convertToGenreNames(movie.genre_ids, genresObject.genres);
   return (
-    <div style={{marginBottom: '20px'}}>
-      <h2>Title: {movie.title}</h2>
-      <h4>Genres: {movieGenreNames}</h4>
-      <h4>Rating: {movie.vote_average}</h4>
-    </div>
+    <Link to={{
+      pathname: '/details',
+      state: {
+        movieObj : movie,
+        genresObj: genresObject
+      }
+    }}>
+      <div style={{marginBottom: '20px'}}>
+        <h2>Title: {movie.title}</h2>
+        <h4>Genres: {movieGenreNames}</h4>
+        <h4>Rating: {movie.vote_average}</h4>
+      </div>
+    </Link>
   )
 }
 
