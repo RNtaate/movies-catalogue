4dea3080f36d7b2c20b29dfa9590d0f44da46dd5
RNtaate <royhyde14@gmail.com>
2021-05-16 04:01:22 +0300

    Add styling to MovieCard component using MovieCard.module.css file

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import {convertToGenreNames} from '../Helpers/HelperMethods';
 import {Link} from 'react-router-dom';
+import * as styles from './MovieCard.module.css'
 
 let MovieCard = (props) => {
+
   const {movie, genresObject} = props;
 
   let movieGenreNames = convertToGenreNames(movie.genre_ids, genresObject.genres);
+
   return (
-    <Link to={{
-      pathname: '/details',
-      state: {
-        movieObj : movie,
-        genresObj: genresObject
-      }
-    }}>
-      <div style={{marginBottom: '20px'}}>
-        <h2>Title: {movie.title}</h2>
-        <h4>Genres: {movieGenreNames}</h4>
-        <h4>Rating: {movie.vote_average}</h4>
-      </div>
-    </Link>
+
+    <div >
+      <Link to={{
+        pathname: '/details',
+        state: {
+          movieObj : movie,
+          genresObj: genresObject
+        }
+      }} className={styles.movieCard_container_div}>
+        <div className={styles.movieCard_poster_div} style={{backgroundImage: `url("https://image.tmdb.org/t/p/w500${movie.poster_path}")`}}></div>
+
+        <div className={styles.movieCard_content_div}>
+          <h2 className={styles.title}>{movie.title}</h2>
+          <div className={styles.rating_genres_div}>
+            <p className={styles.genre}>{movieGenreNames}</p>
+            <span className={styles.rating}><i class="fas fa-star" style={{color: 'orange', marginRight: "5px"}}></i> {Math.round(movie.vote_average)}/10</span>
+          </div>
+        </div>
+      </Link>
+    </div>
+
   )
 }
 
