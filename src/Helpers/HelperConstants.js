55b6bedd6d69c600ca6e1c5f99c8cda0e158389e
RNtaate <royhyde14@gmail.com>
2021-05-21 15:12:42 +0300

    Create getMovieDetailUrl function

--- src/Helpers/HelperConstants.js
+++ src/Helpers/HelperConstants.js
@@ -3,3 +3,5 @@ export const API_KEY = 'd6d714118fedcc0f16131a2c86c27bfa';
 export const getGenreListUrl = (apiKey) => `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`;
 
 export const getMovieListUrl = (apiKey, year, genreList, page = 1) => `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&primary_release_year=${year}&with_genres=${genreList}&with_original_language=en`;
+
+export const getMovieDetailsUrl = (apiKey, movieId) => `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`;
